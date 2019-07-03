d70bfde8ad3c8c472cc259b000bbba00eb07c828
edonadini <43779349+edonadini@users.noreply.github.com>
2019-07-03 15:21:46 +0200

    Update 03_wrong_args.cc

--- lectures/c++/02_functions_and_arrays/03_wrong_args.cc
+++ lectures/c++/02_functions_and_arrays/03_wrong_args.cc
@@ -4,9 +4,18 @@ void swap(int a, int b);
 
 int main() {
   int x{3}, y{7};
-  std::cout << "before\t" << x << " " << y << std::endl;
+  std::cout << "before\t" << x << " " << y << std::endl;//3 7
+  std::cout << "before\t" << &x << " " << &y << std::endl;//0x7ec 0x7f0
+  
+  int t{x};
+  x = y;
+  y = t;
+  std::cout << "after\t" << x << " " << y << " " << t <<std::endl;//7 3 3 ottengo lo swap se non uso la funzione swap
+  std::cout << "after\t" << &x << " " << &y << " " << &t <<std::endl;//0x7ec 0x7f0 0x7f4
+  //prima e dopo lo swap x e y mantengono il proprio indirizzo di memoria
   swap(x, y);
-  std::cout << "after\t" << x << " " << y << std::endl;
+  std::cout << "after\t" << x << " " << y << std::endl;//7 3
+  std::cout << "after\t" << &x << " " << &y <<std::endl;//0x7ec 0x7f0
   return 0;
 }
 
