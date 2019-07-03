ec034edc805b912bd8bff19d04ee85705fac1cf0
edonadini <43779349+edonadini@users.noreply.github.com>
2019-07-03 17:18:46 +0200

    Update 06_static_arrays.cc

--- lectures/c++/02_functions_and_arrays/06_static_arrays.cc
+++ lectures/c++/02_functions_and_arrays/06_static_arrays.cc
@@ -15,17 +15,20 @@ int main() {
   for (std::size_t k{0}; k < 9; ++k)
     std::cout << "af[" << k << "] = " << af[k] << '\n';
 
-  std::cout << "\n how is an array in memory?\n\n";
+  std::cout << "\n how is an array in memory?\n\n"; //i suoi elementi di grandezza definita dal tipo di varibili(4 byte) di cui fanno parte sono tutti vicini in memoria
 
   for (std::size_t k{0}; k < 9; ++k)
     std::cout << "&af[" << k << "] = " << &af[k] << '\n';
 
+  
   std::cout << "\n-------------------------------\n\n";
 
   double ad[7]{1.1, 2.2, 3.3};  // the rest is set to 0
 
   *ad = 9.9;  // an array is implicitly converted to
               // a pointer to its first element;
+  *(ad +2) =7.7; //pointer al terzo elemento
+  *(ad + 8)=8.8; //esco dal range 
   for (std::size_t k{0}; k < 7; ++k)
     std::cout << ad[k] << std::endl;
 
